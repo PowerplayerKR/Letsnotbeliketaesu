c7833d24739cf1100153fe2440cf709d07bc2529
PowerplayerKR <power_player@daum.net>
2018-01-08 21:08:43 +0900

    테스트

--- letsnotbeliketaesu/src/dao/test.java
+++ letsnotbeliketaesu/src/dao/test.java
@@ -2,7 +2,7 @@ package dao;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("하하ㅏ하하");
-		System.out.println("하하하하하");
+		System.out.println("송은이");
+		System.out.println("자은이");
 	}
 }
