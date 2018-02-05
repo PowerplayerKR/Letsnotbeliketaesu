11d10967c8323f49dc9ca849a70a8d983ddb5e71
KOITT <KOITT@KOITT-PC>
2018-02-05 20:15:00 +0900

    d

--- letsnotbeliketaesu/src/controller/BookController.java
+++ letsnotbeliketaesu/src/controller/BookController.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
-import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
