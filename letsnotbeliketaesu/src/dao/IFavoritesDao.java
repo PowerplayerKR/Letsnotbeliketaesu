61208975bc187f97e174b9bd22459c5b430a7a5e
qwea54 <34783184+qwea54@users.noreply.github.com>
2018-03-06 21:02:18 +0900

    Update IFavoritesDao.java

--- letsnotbeliketaesu/src/dao/IFavoritesDao.java
+++ letsnotbeliketaesu/src/dao/IFavoritesDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface IFavoritesDao {
 	public int insertFavorites(HashMap<String,Object> params);
 	public int updateFavorites(HashMap<String,Object> params);
-	public int deleteFavorites(int num);
+	public int deleteFavorites(HashMap<String, Object> params);
 	public HashMap<String,Object> selectFavorites(int num);
 	public List<HashMap<String, Object>> selectAllFavorites();
 }	
