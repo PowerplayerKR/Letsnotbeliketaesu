32c0218c3160e878ddf658d80f38ef7e1a4c0688
miyaomiya <34783211+miyaomiya@users.noreply.github.com>
2018-03-08 20:28:01 +0900

    Update ReviewController.java

--- letsnotbeliketaesu/src/controller/ReviewController.java
+++ letsnotbeliketaesu/src/controller/ReviewController.java
@@ -1,46 +1,63 @@
-package service;
+package controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import service.ReviewListService;
 
-import dao.IReviewListDao;
-
-
-@Service
-public class ReviewListService {
+@Controller
+public class ReviewController {
 	@Autowired
-	private IReviewListDao reviewListDao;
-	public List<HashMap<String, Object>> selectAllReviewList(){
-		return reviewListDao.selectAllReviewList();
+	private ReviewListService reviewListService;
+	
+	@RequestMapping("getReview.do")
+	@ResponseBody
+	public ResponseEntity<String> getReview(@RequestParam String isbn) throws Exception{
+		List<HashMap<String,Object>> reviewList = reviewListService.selectReviewList(isbn);
+		String json = new ObjectMapper().writeValueAsString(reviewList);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.add("Content-Type", "text/html; charset=utf-8");
+		return new ResponseEntity(json, responseHeaders, HttpStatus.CREATED);
 	}
 	
-	public int selectCountReviewList(String isbn){
-		return reviewListDao.selectCountReviewList(isbn);
+	@RequestMapping("getReviewMore.do")
+	@ResponseBody
+	public ResponseEntity<String> getReviewMore(@RequestParam HashMap<String, Object> params) throws Exception{
+		List<HashMap<String,Object>> reviewList = reviewListService.selectReviewListMore(params);
+		List<HashMap<String, Object>> list = new ArrayList<>();
+		for(HashMap<String, Object> map:reviewList) {
+			int rownum = Integer.valueOf(map.get("ROWNUM").toString());
+			int lastNum = Integer.valueOf(params.get("num").toString());
+			if(rownum>lastNum && rownum<=lastNum+5) {
+				list.add(map);
+			}
+		}
+		String json = new ObjectMapper().writeValueAsString(list);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.add("Content-Type", "text/html; charset=utf-8");
+		return new ResponseEntity(json, responseHeaders, HttpStatus.CREATED);
 	}
 	
-	
-	public List<HashMap<String, Object>> selectReviewList(String isbn){
-		return reviewListDao.selectReviewList(isbn);
+	@RequestMapping("getCountReview.do")
+	@ResponseBody
+	public ResponseEntity<String> getCountReview(@RequestParam String isbn) throws Exception{
+		int count = reviewListService.selectCountReviewList(isbn);
+		HashMap<String,Object> map = new HashMap<>();
+		map.put("num",count);
+		String json = new ObjectMapper().writeValueAsString(map);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.add("Content-Type", "text/html; charset=utf-8");
+		return new ResponseEntity(json, responseHeaders, HttpStatus.CREATED);
 	}
 	
-	public List<HashMap<String, Object>> selectReviewListMore(HashMap<String,Object> params){
-		return reviewListDao.selectReviewListMore(params);
-	}
-	
-	public HashMap<String, Object> selectReviewList(int num){
-		return reviewListDao.selectReviewList(num);
-	}
-	
-	public void insertReviewList(HashMap<String, Object> params) {
-		reviewListDao.insertReviewList(params);
-	}
-	public void updateReviewList(HashMap<String, Object> params) {
-		reviewListDao.updateReviewList(params);
-	}
-	public void deleteReviewList(int num) {
-		reviewListDao.deleteReviewList(num);
-	}
 }
