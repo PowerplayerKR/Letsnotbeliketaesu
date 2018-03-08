a4bbe97f27ecf73d05cc83f7de6a3462b6bc61b8
miyaomiya <34783211+miyaomiya@users.noreply.github.com>
2018-03-08 20:27:13 +0900

    Update IReviewListDao.java

--- letsnotbeliketaesu/src/dao/IReviewListDao.java
+++ letsnotbeliketaesu/src/dao/IReviewListDao.java
@@ -9,6 +9,7 @@ public interface IReviewListDao {
 	public int deleteReviewList(int num);
 	public HashMap<String,Object> selectReviewList(int num);
 	public List<HashMap<String, Object>> selectAllReviewList();
+	public int selectCountReviewList(String isbn);
 	public List<HashMap<String, Object>> selectReviewList(String isbn);
 	public List<HashMap<String, Object>> selectReviewListMore(HashMap<String, Object> params);
 }	
